f58b0818126b02e429f892b48f0f3e3526d87ef8
vuvanlinh <vuvanlinh95@gmail.com>
2018-10-08 15:00:25 +0700

    he cac doi tuong hinh hoc

---
+++ src/Shape.java
@@ -0,0 +1,37 @@
+public class Shape {
+    private String color;
+    private boolean filled;
+
+    public Shape() {
+        this.color = "green";
+        this.filled = true;
+
+    }
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = true;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "A Shape with color of " + getColor() + " and" +
+                (isFilled() ? " filled " : " not filled");
+    }
+}
